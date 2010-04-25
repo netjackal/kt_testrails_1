92ab09af098a12c482c957a99cb9763201eb9ee3
Kelvin Teh <kelvin@Ivory.local>
2010-04-26 01:55:51 +0800

    Added custom css to make the scaffold page look better. Modified other files to make use of the css and changed the default layout a little

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+  validates_presence_of :headline, :on => :create, :message => "can't be blank"
+  validates_presence_of :body, :on => :create, :message => "can't be blank"
 end
